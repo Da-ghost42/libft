340ddf151f1ff5413a1e070dea665944cf95eaca
daghost <mboutuil@gmail.com>
2022-11-08 23:45:53 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -21,18 +21,15 @@ int	ft_atoi(const char *str)
 	n = 0;
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
-	while ((*str == '+' || *str == '-'))
+	if (*str == '+' || *str == '-')
 	{
-		if (*str + 1 != '+' && *str + 1 != '-')
-		{
-			if (*str == '-')
+		if (*str == '-')
 				s = s * -1;
-		}
-		else
-			return (0);
 		str++;
 	}
 	while (ft_isdigit(*str) == 1)
 		n = (n * 10) + (*str++ - '0');
 	return (n * s);
 }
+
+
